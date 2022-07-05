4700ec3d7420e2c273901b85d90d03b808897762
Saif <saifrahman169@gmail.com>
2022-07-06 02:24:59 +0530

    Added Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Name} from "./name";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'student-list';
+  nameValue:string="";
+  list:Name[]=[];
+  tag:string="";
+
+  ngOnInit(){
+    this.list=[];
+    this.nameValue="";
+  }
+
+  addName(){
+    if(this.nameValue!=="")
+    {
+      const newItem: Name={
+        id:Date.now(),
+        tag:this.tag,
+        value:this.nameValue,
+        isDone:false
+      };
+      this.list.push(newItem);
+    }
+    this.nameValue="";
+  }
+
+  deleteName(id:Number){
+    this.list=this.list.filter(name=>name.id!==id);
+  }
 }
---
+++ src/app/name.ts
@@ -0,0 +1,6 @@
+export interface Name{
+    id:number;
+    tag:string;
+    value:string;
+    isDone:boolean;
+}
